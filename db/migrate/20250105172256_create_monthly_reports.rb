c20001895f3b7f8a57bf21f570007414d8f2744d
Evgeniy <Vologzhanin2023@gmail.com>
2025-01-05 22:53:34 +0500

    Correction remarks.

--- db/migrate/20250105172256_create_monthly_reports.rb
+++ db/migrate/20250105172256_create_monthly_reports.rb
@@ -1,4 +1,4 @@
-class CreateMonthlyReport < ActiveRecord::Migration[8.0]
+class CreateMonthlyReports < ActiveRecord::Migration[8.0]
   def change
     create_table :monthly_reports do |t|
       t.references :group, null: false, foreign_key: true
