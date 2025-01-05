7b6cfd64c75810f6085468420f19fc025ecaeddd
Evgeniy <Vologzhanin2023@gmail.com>
2025-01-06 00:03:25 +0500

    Enums is overwrited.

--- app/models/info_about_visit.rb
+++ app/models/info_about_visit.rb
@@ -1,11 +1,7 @@
 class InfoAboutVisit < ApplicationRecord
   belongs_to :child
 
-  enum reason: {
-    sick: 0,
-    vacation: 1,
-    other: 2
-  }
+  enum :reason, sick: 0, vacation: 1, other: 2
 
   validates :date, presence: true
   validates :kindergarten_visited, inclusion: { in: [true, false] }
