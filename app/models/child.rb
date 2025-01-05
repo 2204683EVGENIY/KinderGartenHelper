3e56c8e2ec191dfae9e0e15df723c1d12a0fd9fa
Evgeniy <Vologzhanin2023@gmail.com>
2025-01-06 00:18:40 +0500

    Validations are corrected.

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,6 +2,7 @@ class Child < ApplicationRecord
   belongs_to  :group
   has_many :info_about_visits
 
-  validates :first_name, :middle_name, :last_name, :account_number, presence: true
+  validates :first_name, :middle_name, :last_name, :account_number, :active, presence: true
   validates :account_number, numericality: { only_integer: true }, uniqueness: true
+  validates :active, inclusion: { in: [true, false] }
 end
