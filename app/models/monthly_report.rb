d0386a7c7ba779fd2389188d89266b541d13d22a
Evgeniy <Vologzhanin2023@gmail.com>
2025-01-06 16:26:05 +0500

    MonthlyReport has many attached files.

--- app/models/monthly_report.rb
+++ app/models/monthly_report.rb
@@ -2,6 +2,19 @@ class MonthlyReport < ApplicationRecord
   belongs_to :group
   belongs_to :mentor
 
+  has_many_attached :files
+
+  after_create :generate_report_files
+
   validates :report_date, presence: true
   validates :data, presence: true
+
+  private
+
+  def generate_report_files
+    xls_file = generate_xls
+  end
+
+  def generate_xls
+  end
 end
