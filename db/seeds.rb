4c19e11330ca71c250006b7ac9a4e0ca75e9f2fd
Evgeniy <Vologzhanin2023@gmail.com>
2026-04-05 20:44:46 +0500

    Add some snap of future seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,216 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.create(email_address: "galinavologzhanina2@gmail.com", password: "2409Galina1993")
+mentor = Mentor.create(first_name: "Vologzhanina", middle_name: "Galina", last_name: "Nikolaevna", user: user)
+group = Group.create(title: "Фантазёры")
+GroupMentor.create(group: group, mentor: mentor)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
+Child.create(
+  group: group,
+  first_name: "",
+  middle_name: "",
+  last_name: "",
+  account_number:,
+  active: true
+)
